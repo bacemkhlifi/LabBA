c9c60dab222a9599bc0e273988bff45485dde259
bacemkhlifi <bacemkhlifi001@gmail.com>
2021-05-25 22:30:19 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./components/home/Home";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Login from './components/login/Login'
+import Signup from "./components/signup/Signup";
+import Profile from "./components/profil/Profile"
+import EspaceP from "./components/espacePro/EspaceP"
+import LogPro from "./components/espacePro/LogPro"
+import Nav from "./components/navbar/Nav";
+import Conseils from "./components/conseilsPratiques/Conseils";
+import Dashboread from "./components/dashboread/Dashboread";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+
+      <Router>
+         <Nav></Nav>
+        <Switch>
+         
+          <Route exact path="/"component={Home} />
+          <Route exact path="/conseils" component={Conseils}/>
+          <Route exact path="/pro" component={EspaceP}/>
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/loginpro" component={LogPro} />
+          <Route exact path="/signup" component={Signup} />
+          <Route exact path="/profile" component={Profile}/>
+          <Route exact path="/dash" component={Dashboread}/>
+          
+        </Switch>
+      </Router>
     </div>
   );
 }
